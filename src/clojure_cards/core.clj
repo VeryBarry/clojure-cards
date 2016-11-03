70357e57c7a7c165979dfde4efd35b5edde71024
VeryBarry <barry.a.daniels@gmail.com>
2016-11-03 11:58:17 -0400

    added code

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -1,6 +1,28 @@
 (ns clojure-cards.core
   (:gen-class))
 
-(defn -main [& args]
-  (println "Hello, world!"))
+(def suits[:clubs :spades :hearts :diamonds])
+(def ranks(range 1 14))
 
+(defn create-deck []
+  (set
+    (for [suit suits
+          rank ranks]
+         {:suit suit :rank rank})))
+
+(defn create-hands [deck]
+  (set
+    (for [c1 deck
+          c2 (disj deck c1)
+          c3 (disj deck c1 c2)
+          c4 (disj deck c1 c2 c3)]
+      #{c1 c2 c3 c4})))
+
+(defn flush? [hand]
+  (= 1 (count (set (map :suit test-hand)))))
+
+(defn -main [& args]
+  (let [deck (create-deck)
+        hands (create-hands deck)]
+    (println (count hands))))
+    
